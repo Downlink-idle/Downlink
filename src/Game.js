e545ab310b436e1345110a7c684bc2040dbddf51
Wing <eamonn.kearns@so-4pt.net>
2019-08-29 02:43:01 +0100

    Changed the default numbers for Encryption
    Found a bug in the CPU Task assignation that would cause an infinite loop of a task could not be fit on to any of the CPUs

--- src/Game.js
+++ src/Game.js
@@ -16,7 +16,7 @@
         mission:false,
         computer:null,
         downlink:null,
-        version:"0.1.6a",
+        version:"0.1.7a",
         /**
          * jquery entities that are needed for updating
          */
